f8db5bd815d92c8acbccad796ebe3360405e1c41
root-meow <job47@protonmail.ch>
2022-01-11 07:45:07 +0300

    minor bug fixes

--- Teclado30DaysOfPython/TecladoDay12/functions_exercise.py
+++ Teclado30DaysOfPython/TecladoDay12/functions_exercise.py
@@ -34,8 +34,22 @@ multiply(6,5)
 
 #Q2
 
-def print_show_info(tv_show):
-    
+def print_show_info(show):
+    print(f"{show['title']} ({show['initial_release']}) - {show['seasons']} seasons")
+series = [
+{"title": "Breaking Bad", "seasons": 5, "initial_release": 2008},
+{"title": "Fargo", "seasons": 4, "initial_release": 2014},
+{"title": "Firefly", "seasons": 1, "initial_release": 2002},
+{"title": "Rick and Morty", "seasons": 4, "initial_release": 2013},
+{"title": "True Detective", "seasons": 3, "initial_release": 2014},
+{"title": "Westworld", "seasons": 3, "initial_release": 2016},
+]
+
+
+for show in series:
+    print_show_info(show)
+
+
 
 
 #show = {
@@ -43,18 +57,3 @@ def print_show_info(tv_show):
 #	"seasons": 5,
 #	"initial_release": 2008
 #}
-
-    for item in series:
-        print(f"{series['title']} ({series['initial_release']}) - {series['seasons']} seasons")
-
-series = [
- 	{"title": "Breaking Bad", "seasons": 5, "initial_release": 2008},
- 	{"title": "Fargo", "seasons": 4, "initial_release": 2014},
- 	{"title": "Firefly", "seasons": 1, "initial_release": 2002},
- 	{"title": "Rick and Morty", "seasons": 4, "initial_release": 2013},
- 	{"title": "True Detective", "seasons": 3, "initial_release": 2014},
- 	{"title": "Westworld", "seasons": 3, "initial_release": 2016},
- ]
-
-
-print_show_info(series)
