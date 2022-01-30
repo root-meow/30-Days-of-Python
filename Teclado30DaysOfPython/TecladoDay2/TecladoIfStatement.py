82f46a3f67a4d54249e0170acb0f900fc012e579
root-meow <job47@protonmail.ch>
2022-01-30 16:18:05 +0300

    Renamed a variable.

--- Teclado30DaysOfPython/TecladoDay2/TecladoIfStatement.py
+++ Teclado30DaysOfPython/TecladoDay2/TecladoIfStatement.py
@@ -1,9 +1,9 @@
 #Take user input and put that in title case.
 name = input("Please enter your name: ")
-name1 = name.title()
+name_in_Title_case = name.title()
 
 #if statement to make sure you do enter a name.
 if name:
-    print(f"You have entered {name1}")
+    print(f"You have entered {name_in_Title_case}")
 else:
     print("To proceed you must enter a name! ")
