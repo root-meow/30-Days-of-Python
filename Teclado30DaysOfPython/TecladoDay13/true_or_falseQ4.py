f70a20a273f8ac14377066f61ddd53b761ecde52
root-meow <job47@protonmail.ch>
2022-02-15 15:03:22 +0300

    tiny tidying up changes

--- Teclado30DaysOfPython/TecladoDay13/true_or_falseQ4.py
+++ Teclado30DaysOfPython/TecladoDay13/true_or_falseQ4.py
@@ -1,19 +1,17 @@
 
 def prime_finder(dividend):
-        # Get a number to test from the user, and set the initial divisor to 2
-    #dividend = int(input("Please enter a number: "))
     divisor = 2
 
     # Keep looping until the divisor equals the number we're testing
     while divisor < dividend:
         # If user's number is divisible by the curent divisor, break the loop
         if dividend % divisor == 0:
-            print("false")
+            return "false"
             break
 
         # Increment the divisor for the next iteration
         divisor = divisor + 1
     else:
         # This line only runs if no divisors produced integer results
-        print("True")
+        return "True"
 print(prime_finder(7))
